70242a38119e1d82a5aa49e954bfe4df7198ef70
wojlin <wojciech.linowski.2137@gmail.com>
2023-06-27 15:33:00 +0200

    implementing stops functionality

--- static/js/app.js
+++ static/js/app.js
@@ -76,22 +76,73 @@ class App{
         console.log("initialized app!");
         this.session = new Session();
         this.UserInterfaceManager = new UserInterfaceManager();
+        this.requester = new Requester();
 
         this.initialDataInfo = {
                                     0: {"full_name": "stops data", "name": "stops_data", "expired_time": 10, "url": "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/d3e96eb6-25ad-4d6c-8651-b1eb39155945/download/stopsingdansk.json"}
                                };
 
         this.fetch_delay = 1000;
+        
+        this.map = null;
+
+        this.mapLayers = {"stop": [],
+                          "tram": [],
+                           "bus": [],
+                           "other": []
+                        }
 
         
     }
 
     start()
     {
+        this.createMap();
         this.checkInitialData();
         this.fetchInitialData();
 
+        this.displayStops();
+
         console.log("end")
+    }
+
+    createMap(){
+        this.map = L.map('map').fitWorld();
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: ''
+        }).addTo(this.map);
+
+
+        this.map.setView([54.3520500, 18.6463700], 12);
+
+        let marker = L.marker([54.43483479706698, 18.624774574836078], {icon: stopIcon}).addTo(this.map);
+    }
+
+    displayStops(){   
+        let stops_data = this.session.get("stops_data");
+        console.log(stops_data);
+        for(let entry in stops_data["stops"])
+        {
+            entry = stops_data["stops"][entry];
+            let name = entry["stopName"];
+            let lat = entry["stopLat"];
+            let lon = entry["stopLon"];
+            let demand = entry["onDemand"];
+            let code = entry["stopCode"];
+            let stopId = entry["stopId"];
+
+            let desc = "<p class='stop_name' id='"+stopId+"' data-id='"+stopId+"' >" + name + " " + code + "</p>";
+            if(demand == true)
+            {
+                desc += "<p style='text-align:center;'>(Na żądanie)</p>"
+            }
+            let marker = L.marker([lat, lon], {icon: stopIcon}).addTo(this.map);
+            marker.bindPopup(desc);
+            this.mapLayers["stop"].push(marker);
+
+        }
     }
 
     checkInitialData(){
@@ -144,9 +195,6 @@ class App{
             current_index = 0;
         }
 
-        console.log(current_index)
-
-        console.log(app.initialDataInfo)
         if(current_index >= Object.keys(app.initialDataInfo).length)
         {
             console.log("hide");
@@ -175,7 +223,7 @@ class App{
                 app.saveData(entry["name"], JSON.parse(xmlHttp.responseText));
                 app.saveData("stops_data_time", new Date().getTime());
 
-                if(current_index >= Object.keys(app.initialDataInfo).length)
+                if(current_index +1 >= Object.keys(app.initialDataInfo).length)
                 {
                     console.log("hide")
                     app.UserInterfaceManager.hideFetchingScreenDelay();
@@ -200,13 +248,50 @@ var app = new App();
 app.start();
 
 
+app.map.on('popupopen', function(e) {
+    let stopID = e.popup._contentNode.firstChild.dataset.id;
 
-var map = L.map('map').fitWorld();
+    function get(element, url)
+    {
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            {
+                let response = JSON.parse(xmlHttp.responseText);
+                console.log(response);
+                if(Object.keys(response["departures"]).length == 0){
+                    e.popup.setContent(e.popup._content + "<p>Brak Odjazdów</p>");
+                    return;
+                }
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: ''
-}).addTo(map);
+                let table = "<table><tr><th>nazwa linii</th><th>numer</th><th>czas</th><th>status</th></tr>";
+                for(let entry in response["departures"])
+                {
+                    entry = response["departures"][entry];
+                    let status = "<img style='width:2vw;' src='static/images/"
+                    if(entry["status"] == "REALTIME"){
+                        status += "realtime_status.png";
+                    }
+                    else if(entry["status"] == "SCHEDULED"){
+                        status += "scheduled_status.png";
+                    }
+                    else{
+                        status += "unknown_status.png";
+                    }
+                    status += "' alt='status'>"
 
+                    table += "<tr><td>"+entry["headsign"]+"</td><td>"+entry["routeId"]+"</td><td>"+entry["estimatedTime"]+"</td><td>"+status+"</td></tr>";
+                }
+                table += "</table>"
+                e.popup.setContent(e.popup._content + table);
+            }               
+        }
+        xmlHttp.open("GET", url, true);
+        xmlHttp.send(null);
+    }
 
-map.setView([54.3520500, 18.6463700], 12);
+    get(e, "http://ckan2.multimediagdansk.pl/departures?stopId="+ stopID, )
+    
+    
+  });
+  
---
+++ static/js/utils.js
@@ -0,0 +1,10 @@
+var stopIcon = L.icon({
+    iconUrl: 'static/images/stop.png',
+    shadowUrl: 'static/images/stop_shadow.png',
+
+    iconSize:     [35, 35], // size of the icon
+    shadowSize:   [35, 35], // size of the shadow
+    iconAnchor:   [7.5, 7.5], // point of the icon which will correspond to marker's location
+    shadowAnchor: [6, 6],  // the same for the shadow
+    popupAnchor:  [7.5, -15] // point from which the popup should open relative to the iconAnchor
+});
